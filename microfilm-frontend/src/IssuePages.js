aeb22635e62ebc524213f8ecff8f796efed9f304
github username <github email address>
2020-06-18 10:45:38 -0600

    pdf pages showing in app

--- microfilm-frontend/src/IssuePages.js
+++ microfilm-frontend/src/IssuePages.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-export default function IssueDetails ( {image} ) {
+export default function IssuePages ( {image} ) {
 
-    const proxy = `https://cors-anywhere.herokuapp.com/`
+    // const proxy = `https://cors-anywhere.herokuapp.com/`
 
-    const fetchImage = () => {
-        fetch(`${proxy}${image.pdf}`)
-            .then(response => response.json())
-            .then(result => displayImage(result.pdf))
-    }
+    // const fetchImage = () => {
+    //     fetch(`${proxy}${image.pdf}`)
+    //         .then(response => response.json())
+    //         .then(result => displayImage(result.pdf))
+    // }
 
-const displayImage = (pdf) => <img src={pdf} alt={`page ${image.sequence}`}></img>
+const displayImage = () => <embed src={image.pdf} width="800px" height="2100px" />
 
     return (
         <div>
-            {fetchImage()}
+            {/* {fetchImage()} */}
+            {displayImage()}
         </div>
     )
 }
